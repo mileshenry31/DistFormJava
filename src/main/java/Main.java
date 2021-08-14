a707518e1ca007f977704452b50b27f8e4170051
Miles Henry <milesmhenry@icloud.com>
2021-08-14 12:49:04 -0700

    Added the Polish Cow song when the math is done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
 import javax.swing.JOptionPane;
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
+        File file = new File("Polish cow (English Lyrics Full Version) (128 kbps).wav");
+        AudioInputStream stream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(stream);
         float x1 = Float.parseFloat(JOptionPane.showInputDialog("What is x1"));
         float x2 = Float.parseFloat(JOptionPane.showInputDialog("What is x2"));
         float y1 = Float.parseFloat(JOptionPane.showInputDialog("What is y1"));
@@ -9,7 +17,11 @@ public class Main {
         float formula = (float) Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
         float midpointx = (x1 + x2) / 2;
         float midpointy = (y1 + y2) / 2;
+
+        clip.start();
+        
         JOptionPane.showMessageDialog(null,"The answer is " + formula);
         JOptionPane.showMessageDialog(null,"The midpoint is " + "(" + midpointx + ", " + midpointy + ")");
+        
     }
 }
